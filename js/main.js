b21bfc4b736d092437d8b0886eb3857e2493e527
Sanchez <bradsanchez9845@gmail.com>
2023-06-29 16:55:06 +0200

    Start

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let userName = prompt("Nome: ");
+let userSurname = prompt("Cognome: ");
+let userFavoriteColor = prompt("Colore Preferito: ");
+let firtsNumber = parseInt(prompt ("Primo numero: "));
+let secondNumber = parseInt(prompt ("Secondo numero: "));
+
+let passwordNumber = firtsNumber / secondNumber;
+
+let userRandomPassword = userName + userSurname + userFavoriteColor + passwordNumber;
+
+console.log(userRandomPassword);
+
+document.getElementById("randomPassword").innerHTML = userRandomPassword;
+
+document.getElementById("userName").innerHTML = userName;
